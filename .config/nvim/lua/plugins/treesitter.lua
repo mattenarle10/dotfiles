e5a6ac45c6a7cc3130527574e5eab59a96796e90
mattenarle10 <enarlem10@gmail.com>
2026-05-07 11:45:19 +0800

    fix(nvim): use lazy main+opts for treesitter to fix load order error

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,18 +2,17 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua", "vim", "vimdoc",
-        "javascript", "typescript", "tsx",
-        "python", "rust", "go",
-        "html", "css", "json", "yaml",
-        "markdown", "markdown_inline",
-        "bash",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "lua", "vim", "vimdoc",
+      "javascript", "typescript", "tsx",
+      "python", "rust", "go",
+      "html", "css", "json", "yaml",
+      "markdown", "markdown_inline",
+      "bash",
+    },
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
